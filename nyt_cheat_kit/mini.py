571c0ee24c9fd3a41e74ec50b4f748d373500dde
Andrew K DeCotiis-Mauro <andrewkmauro@gmail.com>
2024-04-26 13:58:28 -0400

    formatting with black and flake8

--- nyt_cheat_kit/mini.py
+++ nyt_cheat_kit/mini.py
@@ -7,36 +7,38 @@ today = today.strftime("%b %-d, %Y")
 
 print(today)
 
-answers = 'https://word.tips/todays-nyt-mini-crossword-clues-answers/'
+answers = "https://word.tips/todays-nyt-mini-crossword-clues-answers/"
 across_id = "Across Clues and Answers for Today's Puzzle"
 down_class = "text-[1.5rem] mb-5 leading-[38px]"
-across_mark = '********** ACROSS **********'
-down_mark = '********** DOWN **********'
+across_mark = "********** ACROSS **********"
+down_mark = "********** DOWN **********"
 
-headers = {'user-agent': 'nyt-cheat-kit/0.0.1'}
-r = requests.get(answers,headers=headers)
+headers = {"user-agent": "nyt-cheat-kit/0.0.1"}
+r = requests.get(answers, headers=headers)
 
 if r.status_code != 200:
-    print(f'Error with site: {r.status_code}')
+    print(f"Error with site: {r.status_code}")
 
-soup = bs(r.text, 'html.parser')
+soup = bs(r.text, "html.parser")
 
-across = soup.find('div', {'id': across_id})
+across = soup.find("div", {"id": across_id})
 
-clues = across.findChildren('h3')
+clues = across.findChildren("h3")
 all_answers = [a.next_sibling.text for a in clues]
 
 # sort
-across_answers = [a for idx, a in enumerate(all_answers)
-                  if idx < all_answers.index('') and a != '']
+across_answers = [
+    a for idx, a in enumerate(all_answers) if idx < all_answers.index("") and a != ""
+]
 
-down_answers = [a for idx, a in enumerate(all_answers)
-                  if idx > all_answers.index('') and a != '']
+down_answers = [
+    a for idx, a in enumerate(all_answers) if idx > all_answers.index("") and a != ""
+]
 
 print(across_mark)
 for ans in across_answers:
-    print(ans.split(' ')[-1].replace('.',''))
+    print(ans.split(" ")[-1].replace(".", ""))
 
 print(down_mark)
 for ans in down_answers:
-    print(ans.split(' ')[-1].replace('.',''))
+    print(ans.split(" ")[-1].replace(".", ""))
--- nyt_cheat_kit/wordle.py
+++ nyt_cheat_kit/wordle.py
@@ -1,33 +1,31 @@
 import requests
 from bs4 import BeautifulSoup as bs
-from datetime import datetime, timedelta
-
+from datetime import datetime
 
 today = datetime.today()
 wordle_1 = datetime(2021, 6, 19, 0, 0, 0)
 wordle_number = today - wordle_1
 
-answers = 'https://www.tomsguide.com/news/what-is-todays-wordle-answer'
+answers = "https://www.tomsguide.com/news/what-is-todays-wordle-answer"
 
-headers = {'user-agent': 'nyt-cheat-kit/0.0.1'}
-r = requests.get(answers,headers=headers)
+headers = {"user-agent": "nyt-cheat-kit/0.0.1"}
+r = requests.get(answers, headers=headers)
 
 if r.status_code != 200:
-    print(f'Error with site: {r.status_code}')
+    print(f"Error with site: {r.status_code}")
 
-soup = bs(r.text, 'html.parser')
+soup = bs(r.text, "html.parser")
 
 print(f'{today.strftime("%b %-d, %Y")} - Wordle #{wordle_number.days}\n')
 
 # <strong> text surrounds a hint on the first letter and the answer
 # they are the final two uses of <strong>
-bold = soup.find_all('strong')[-2:]
+bold = soup.find_all("strong")[-2:]
 
-if input('Do you want a hint? (y/n) ') == 'y':
-    print(bold[0].text[:-1] + '\n')
+if input("Do you want a hint? (y/n) ") == "y":
+    print(bold[0].text[:-1] + "\n")
 else:
-    print('\n')
+    print("\n")
 
-if input('You sure you want the answer? (y/n) ') == 'y':
+if input("You sure you want the answer? (y/n) ") == "y":
     print(bold[1].text)
-
