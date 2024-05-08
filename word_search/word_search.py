7d3da022b2a5417892c460ef75b0c42f62b9b618
Andrew K DeCotiis-Mauro <andrewkmauro@gmail.com>
2024-05-08 11:55:27 -0400

    gave CLI for usage.

--- word_search/word_search.py
+++ word_search/word_search.py
@@ -1,49 +1,117 @@
-word_length = input("How many letters is the word? ")
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog="word_search",
+    description=(
+        "Helps you analyse wordle guesses,"
+        " or reduce the space of wordle/crossword clues."
+    ),
+)
+parser.add_argument(
+    "-n", "--num", type=int, help="Number of letters in the word.", required=False
+)
+parser.add_argument(
+    "-c",
+    "--correct",
+    help="Put known letters in the word in their"
+    " location separated by `-` for unknowns.",
+    required=False,
+)
+parser.add_argument(
+    "-i", "--included", help="Provide included letters.", required=False
+)
+parser.add_argument(
+    "-d",
+    "--disallowed",
+    help="Provide letters that are not in the word.",
+    required=False,
+)
+parser.add_argument(
+    "-ws",
+    "--wrong-spot",
+    help="Provide letters that are not allowed in a location."
+    " Pass as a comma-separated list.",
+    type=lambda x: x.split(","),
+    required=False,
+)
+parser.add_argument(
+    "-chkpt",
+    "--checkpoint",
+    action="store_true",
+    help="Bool for if checkpoints should be saved.",
+    default=False,
+    required=False,
+)
+args = parser.parse_args()
+
+if args.num:
+    word_length = args.num
+else:
+    word_length = input("How many letters is the word? ")
 
 
-with open('dicts/dict.txt') as file:
-  n_letter_words = []
-  for line in file.readlines():
-    if len(line.strip()) == int(word_length):
-      n_letter_words.append(line.lower().strip())
+with open("dicts/dict.txt") as file:
+    n_letter_words = []
+    for line in file.readlines():
+        if len(line.strip()) == int(word_length):
+            n_letter_words.append(line.lower().strip())
 
-print(f'There are {len(n_letter_words)} words in the dictionary with'
-       f' {word_length} letters.')
+print(
+    f"There are {len(n_letter_words)} words in the dictionary with"
+    f" {word_length} letters."
+)
 
-with open(f'dicts/n_letters-{word_length}.txt', 'w') as file:
-    file.writelines([f'{n}\n' for n in n_letter_words])
+if args.checkpoint:
+    with open(f"dicts/n_letters-{word_length}.txt", "w") as file:
+        file.writelines([f"{n}\n" for n in n_letter_words])
 
 wrong_locations = {i: None for i in range(int(word_length))}
 correct_locations = {i: None for i in range(int(word_length))}
 
-included_letters = set(list(input('What letters in the word? ')))
-excluded_letters = set(list(input('What letters are not in the word? ')))
-knowns = input('Known letters? Use - for unknowns. ')
+if args.included:
+    included_letters = args.included
+else:
+    included_letters = input("What letters in the word? ")
+if args.disallowed:
+    excluded_letters = args.disallowed
+else:
+    excluded_letters = set(list(input("What letters are not in the word? ")))
+
+included_letters = set(list(included_letters))
+excluded_letters = set(list(excluded_letters))
+
+if args.correct:
+    knowns = args.correct
+else:
+    knowns = input("Known letters? Use - for unknowns. ")
 
 # input checks
 while True:
     if len(knowns) == int(word_length):
         break
-    print(f'{len(knowns)} letters entered. Please try again.')
-    knowns = input('Known letters? Use - for unknowns. ')
+    print(f"{len(knowns)} letters entered. Please try again.")
+    knowns = input("Known letters? Use - for unknowns. ")
 
 if not included_letters.isdisjoint(excluded_letters):
-    print('Overlap between allowed and disallowed letters!')
+    print("Overlap between allowed and disallowed letters!")
     overlap = excluded_letters.intersection(included_letters)
-    print(f'Removing {overlap} from disallowed.')
+    print(f"Removing {overlap} from disallowed.")
     excluded_letters -= overlap
 
-
-for idx in wrong_locations.keys():
-    wrong_locations[idx] = list(
-        input(f'Not allowed in {idx + 1}? (Enter to skip): ')
+if args.wrong_spot and len(args.wrong_spot) == word_length:
+    for idx in wrong_locations.keys():
+        wrong_locations[idx] = list(args.wrong_spot[idx])
+else:
+    for idx in wrong_locations.keys():
+        wrong_locations[idx] = list(
+            input(f"Not allowed in {idx + 1}? (Enter to skip): ")
         )
 
 for idx, known in enumerate(list(knowns)):
-    if known != '-':
+    if known != "-":
         correct_locations[idx] = known
 
-print(f'included: {included_letters}\nexcluded: {excluded_letters}')
+print(f"included: {included_letters}\nexcluded: {excluded_letters}")
 
 allowed_words = []
 
@@ -51,8 +119,9 @@ if len(included_letters) == 0:
     allowed_words = n_letter_words.copy()
 else:
     for i, word in enumerate(n_letter_words):
-        if (included_letters.issubset(set(word)) and \
-           set(word).isdisjoint(excluded_letters)):
+        if included_letters.issubset(set(word)) and set(word).isdisjoint(
+            excluded_letters
+        ):
             allowed_words.append(word)
 
 filtered_words = []
@@ -79,26 +148,32 @@ for word in allowed_words:
 # remove dups
 filtered_words = list(set(filtered_words))
 
-with open(f'allowed/allowed_words-len_{word_length}_'
-            f'inc_{"".join(included_letters)}-exc_'
-            f'{"".join(excluded_letters)}.txt', 'w') as file:
-    file.writelines([f'{n}\n' for n in allowed_words])
+if args.checkpoint:
+    with open(
+        f"allowed/allowed_words-len_{word_length}_"
+        f'inc_{"".join(included_letters)}-exc_'
+        f'{"".join(excluded_letters)}.txt',
+        "w",
+    ) as file:
+        file.writelines([f"{n}\n" for n in allowed_words])
 
-label = 'fil_'
-for k,v in wrong_locations.items():
-    label += str(k + 1)+''.join(v)
-label += 'corr_'
-for k,v in correct_locations.items():
-    if v:
-        label += str(k+1) + v
+    label = "fil_"
+    for k, v in wrong_locations.items():
+        label += str(k + 1) + "".join(v)
+    label += "corr_"
+    for k, v in correct_locations.items():
+        if v:
+            label += str(k + 1) + v
 
+    with open(
+        f"filtered/filtered_words-len_{word_length}_"
+        f'inc_{"".join(included_letters)}-exc_'
+        f'{"".join(excluded_letters)}-{label}.txt',
+        "w",
+    ) as file:
+        file.writelines([f"{n}\n" for n in filtered_words])
 
-with open(f'filtered/filtered_words-len_{word_length}_'
-            f'inc_{"".join(included_letters)}-exc_'
-            f'{"".join(excluded_letters)}-{label}.txt', 'w') as file:
-    file.writelines([f'{n}\n' for n in filtered_words])
+print(f"There are {len(filtered_words)} remaining.")
 
-print(f'There are {len(filtered_words)} remaining.')
-
-if input('Do you want to see the remaining words? (y/n) ').lower() == 'y':
-    print('\n'.join(filtered_words))
+if input("Do you want to see the remaining words? (y/n) ").lower() == "y":
+    print("\n".join(filtered_words))
